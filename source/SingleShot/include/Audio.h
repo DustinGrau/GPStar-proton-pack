513b1d358d43fbfd9dc4c43fab0b272c4a6a6026
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-09-04 17:09:44 -0400

    Update for new audio libs

--- source/SingleShot/include/Audio.h
+++ source/SingleShot/include/Audio.h
@@ -726,9 +726,15 @@ bool setupAudioDevice() {
 
   char gVersion[VERSION_STRING_LEN];
 
+  // Configure to use the hardware Serial3 port.
+  Serial3.begin(57600);
   audio.start(Serial3);
 
   delay(10);
+
+  // Ask for some Wav Trigger information.
+  audio.requestVersionString();
+  audio.requestSystemInfo();
 
   // Stop all tracks.
   audio.stopAllTracks();
--- source/SingleShot/src/main.cpp
+++ source/SingleShot/src/main.cpp
@@ -27,7 +27,7 @@
 #endif
 
 // Set to 1 to enable built-in debug messages
-#define DEBUG 0
+#define DEBUG 1
 
 // Debug macros
 #if DEBUG == 1
