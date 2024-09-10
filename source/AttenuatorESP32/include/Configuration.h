a2f8202ff1e92678727250c19e824471788fe4c8
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-09-10 18:09:17 -0400

    Changing the way we save to EEPROM

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240910152904";
+String build_date = "20240910180035";
 
 /*
  * Preferred WiFi Network Defaults
