5de5f5d499a28778fa715d0426dc04cb62a19e43
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-09-11 19:40:17 -0400

    Use all 7 LEDs for the new blaster barrel

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20240910180035";
+String build_date = "20240911193437";
 
 /*
  * Preferred WiFi Network Defaults
