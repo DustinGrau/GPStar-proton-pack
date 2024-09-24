b52610ca413756deb2abda19876ac24c03098f99
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-09-24 09:28:42 -0400

    Address use of modern PlatformIO libraries

--- source/AttenuatorESP32/src/main.cpp
+++ source/AttenuatorESP32/src/main.cpp
@@ -147,13 +147,8 @@ void setup() {
 
   // Feedback devices (piezo buzzer and vibration motor)
   pinMode(BUZZER_PIN, OUTPUT);
-  // Note: "ledcAttach" is a combined method for the arduino-esp32 v3.x board library
-  //ledcAttach(VIBRATION_PIN, 5000, 8); // Uses 5 kHz frequency, 8-bit resolution
-
-  // Attach pin to a channel (we'll use the same # as the vibration pin for consistency)
-  ledcAttachPin(VIBRATION_PIN, VIBRATION_PIN);
-  // Configure LEDC PWM channel (using a consistent value, the vibration pin #)
-  ledcSetup(VIBRATION_PIN, 5000, 8);
+  // Use the combined method for the arduino-esp32 platform, using the esp-idf v5.3+
+  ledcAttach(VIBRATION_PIN, 5000, 8); // Uses 5 kHz frequency, 8-bit resolution
 
   // Turn off any user feedback.
   noTone(BUZZER_PIN);
