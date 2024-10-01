4441aa285628d14ce2734151500a85364504139f
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-10-01 12:36:08 -0400

    Change status code for system updates

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20241001110647";
+String build_date = "20241001123311";
 
 /*
  * Preferred WiFi Network Defaults
