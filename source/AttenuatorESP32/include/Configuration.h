5de71588a85118a36ec589fecc074deef3cd06ef
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2024-10-07 13:18:22 -0400

    Shrink SVG file

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -44,7 +44,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20241006124153";
+String build_date = "20241007130425";
 
 /*
  * Preferred WiFi Network Defaults
