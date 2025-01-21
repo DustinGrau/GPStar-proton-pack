501fa4f3c5c37a8b297dcf3be825d5fe4e3a84a6
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-01-21 06:49:02 -0500

    Recompile all binaries

--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250108093650";
+String build_date = "20250121064439";
 
 /*
  * Parent WiFi Network Defaults
