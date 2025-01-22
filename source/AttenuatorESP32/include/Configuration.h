92cf52bf0806e692f245c4db60ce6dd549025c93
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-01-22 10:06:40 -0500

    Sync changes from 5.4 branch, rebuild with new libraries

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250121203457";
+String build_date = "20250122100207";
 
 /*
  * Preferred WiFi Network Defaults
--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250121064439";
+String build_date = "20250122100259";
 
 /*
  * Parent WiFi Network Defaults
