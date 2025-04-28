18b84634b7413a18744978ee2d64294056d6cf34
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-04-28 06:36:53 -0400

    Update from 5.4.4 code

--- source/AttenuatorESP32/include/Configuration.h
+++ source/AttenuatorESP32/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250427073845";
+String build_date = "V6_20250428063540";
 
 /*
  * Preferred WiFi Network Defaults
--- source/GhostTrap/include/Configuration.h
+++ source/GhostTrap/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250427073931";
+String build_date = "20250428063626";
 
 /*
  * Preferred WiFi Network Defaults
