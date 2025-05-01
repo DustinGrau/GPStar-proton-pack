16a5d61b16cbed060aaebc262221dc812672cc00
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-05-01 06:55:59 -0400

    Sync fix for ESP issues

--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250430063555";
+String build_date = "20250501064803";
 
 /*
  * Parent WiFi Network Defaults
