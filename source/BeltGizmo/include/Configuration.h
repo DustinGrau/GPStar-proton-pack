798d2f57d0f5ba70368ffe9fda48ea014f94600b
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-05-25 08:42:26 -0400

    Update libraries, recompile

--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250522065753";
+String build_date = "20250525084034";
 
 /*
  * Parent WiFi Network Defaults
--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250522065803";
+String build_date = "20250525084049";
 
 /*
  * Parent WiFi Network Defaults
