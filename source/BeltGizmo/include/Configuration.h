25ce6cda4d671d7874c7bcf4a5c8dd81466e49f7
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-06-08 13:15:20 -0400

    Revert "Sync from 6.1 changes"
    This reverts commit 8697cfe775ec88b965ce03270c131716a0fbc888.

--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250608110004";
+String build_date = "20250608105509";
 
 /*
  * Parent WiFi Network Defaults
--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250608110015";
+String build_date = "20250608105554";
 
 /*
  * Parent WiFi Network Defaults
