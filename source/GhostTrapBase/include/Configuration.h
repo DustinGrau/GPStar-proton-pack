8697cfe775ec88b965ce03270c131716a0fbc888
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-06-08 11:00:40 -0400

    Sync from 6.1 changes

--- source/GhostTrapBase/include/Configuration.h
+++ source/GhostTrapBase/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "20250608104934";
+String build_date = "20250608110026";
 
 /*
  * Preferred WiFi Network Defaults
