74f59ee0fbdf865ac9c2ce5bfcf3065f39c2ed84
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-08-08 19:13:08 -0400

    Merge changes from 6.0 fixes

--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250730200730";
+String build_date = "V6_20250808173705";
 
 /*
  * Parent WiFi Network Defaults
