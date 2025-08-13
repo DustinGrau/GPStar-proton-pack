8141a1472032fea4bbffbf10c2a0496452327bbd
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-08-13 17:24:35 -0400

    Rebuild binaries

--- source/GhostTrapBase/include/Configuration.h
+++ source/GhostTrapBase/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250812205434";
+String build_date = "V6_20250813170106";
 
 /*
  * Preferred WiFi Network Defaults
