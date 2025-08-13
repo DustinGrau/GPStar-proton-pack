39d1e054bb0f0df3f1f84b9504dd3071fc0d2aca
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-08-13 17:01:20 -0400

    Integrate new libraries for sensors

--- source/SingleShot/include/Configuration.h
+++ source/SingleShot/include/Configuration.h
@@ -23,7 +23,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250812205434";
+String build_date = "V6_20250813165757";
 
 /*
  * Preferred WiFi Network Defaults (only for ESP32)
