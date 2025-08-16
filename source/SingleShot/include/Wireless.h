c3fc6806bf4453c3a271b570b7520cb2680c3f91
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-08-16 15:30:29 -0400

    Cleanup

--- source/SingleShot/include/Wireless.h
+++ source/SingleShot/include/Wireless.h
@@ -47,7 +47,7 @@
 
 // Set up values for the SSID and password for the built-in WiFi access point (AP).
 const uint8_t i_max_attempts = 3; // Max attempts to establish a external WiFi connection.
-const String ap_default_ssid = "GPStarBlaster"; // This will be the base of the SSID name.
+const String ap_default_ssid = "GPStar_Blaster"; // This will be the base of the SSID name.
 String ap_default_passwd = "555-2368"; // This will be the default password for the AP.
 String ap_ssid; // Reserved for holding the full, private AP name for this device.
 bool b_ap_started = false; // Denotes the softAP network has been started.
