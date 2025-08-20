6bd1e60cd1fb6a3bd3acda17b01d908191d5654c
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-08-20 15:56:34 -0400

    Turn off motion calibration, adjust naming

--- source/NeutronaWand/include/System.h
+++ source/NeutronaWand/include/System.h
@@ -1509,7 +1509,6 @@ void postActivation(bool shortBoot = false) {
 #ifdef ESP32
   // When the wand is activated we should consider the user's current orientation by
   // way of holding of the device as the new reference position for motion tracking.
-  debugln(F("postActivation() - Resetting all motion data."));
   resetAllMotionData();
 #endif
 
