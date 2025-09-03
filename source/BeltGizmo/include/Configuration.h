bcffbb97580de98e0b9977270c5c1183ba7e78a9
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-09-02 20:54:48 -0400

    Add support for theme change API

--- source/BeltGizmo/include/Configuration.h
+++ source/BeltGizmo/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250827134444";
+String build_date = "V6_20250902204745";
 
 /*
  * Parent WiFi Network Defaults
