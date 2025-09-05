1000e997b56f66a9e2354b4d81e2cf9619e6a833
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-09-05 11:33:08 -0400

    Change from String to char[] where possible

--- source/GhostTrapBase/include/Configuration.h
+++ source/GhostTrapBase/include/Configuration.h
@@ -45,7 +45,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250905065213";
+String build_date = "V6_20250905112927";
 
 /*
  * Preferred WiFi Network Defaults
