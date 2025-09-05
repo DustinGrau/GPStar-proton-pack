f3299e80848e149fb8871c92dbc83ac658af6054
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-09-05 10:50:40 -0400

    Make strings more obvious when used as constants

--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250905065213";
+String build_date = "V6_20250905104926";
 
 /*
  * Parent WiFi Network Defaults
