8c709792fd9309dc8fe6c5e977b79774bfec834b
Dustin Grau <115430732+DustinGrau@users.noreply.github.com>
2025-09-17 12:58:09 -0400

    Update binary

--- source/StreamEffects/include/Configuration.h
+++ source/StreamEffects/include/Configuration.h
@@ -43,7 +43,7 @@
 /*
  * Used to reflect the last build date for the binary.
  */
-String build_date = "V6_20250917080121";
+String build_date = "V6_20250917125551";
 
 /*
  * Parent WiFi Network Defaults
